ea5129b15c76c7f9c902c3e0d2aed068a91d8a06
bryce <bryjaco@ucdavis.edu>
2021-08-19 13:34:12 -0700

    first project version

---
+++ learning_logs/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Topic
+
+# Create your views here.
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
+def topics(request): # topics() func takes request obj as param.
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added') # this is a db query. It's so clean!!
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
