b2f08de85005f1e76f34429ee1aaa29645489562
Bryce <bryjaco@ucdavis.edu>
2021-09-08 01:11:17 +0000

    learning_log is fully functional

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,22 +2,37 @@ from django.shortcuts import render, redirect
 # import classes from the .py files that I wrote in the learning_logs dir.
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
+# func for check if user has perms
+def check_topic_owner(request, topic):
+    if request.user != topic.owner:
+        raise Http404
 
 # Create your views here.
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
+
+@login_required
 def topics(request): # topics() func takes request obj as param.
     """Show all topics."""
-    topics = Topic.objects.order_by('date_added') # this is a db query. It's so clean!!
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added') # this is a db query. It's so clean!!
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    # stop other users from accessing user specific content.
+    check_topic_owner(request, topic)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+@login_required
 def new_topic(request):
     """Add new topic"""
     if request.method != 'POST':
@@ -27,19 +42,24 @@ def new_topic(request):
         # process the submitted data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
+            # i feel like this is a cheeky hack to edit fields on a form.
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
             # create entry in db with data in form.
-            form.save()
+            new_topic.save()
             # upon saveing their entry, redirect user to topics page.
             return redirect('learning_logs:topics')
-
     # if the request was not to post or the form was invalid, display blank or invalid form
     context = {'form': form}
     # pass the request obj into the right func in views.py and also feed in either
     # a blank form, or the invalid form that the user submitted
     return render(request, 'learning_logs/new_topic.html', context)
+
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(request, topic)
     if request.method != 'POST':
         # No data submitted; create a blank form.
         form = EntryForm()
@@ -59,10 +79,13 @@ def new_entry(request, topic_id):
         # Display a blank or invalid form.
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(request, topic)
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
         form = EntryForm(instance=entry)
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
 """Defines URL patterns for users"""
 from django.urls import path, include
+# from the current app (users), import views.py
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # Include default auth urls so we can use them under base/users/
     path('', include('django.contrib.auth.urls')),
+    # Registration page.
+    path('register/', views.register, name='register'),
 ]
